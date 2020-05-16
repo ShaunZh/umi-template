694bdf9eb094b9adbee8d30a24daa620d0a78c06
wang jian <542236125@qq.com>
2020-05-16 15:48:12 +0800

    直播静态页面开发

--- src/pages/live/create/index.tsx
+++ src/pages/live/create/index.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import styles from './index.less';
-import {
-  Form,
-  Select,
-  InputNumber,
-  Switch,
-  Radio,
-  Slider,
-  Button,
-  Upload,
-  Rate,
-  Checkbox,
-  Row,
-  Col,
-} from 'antd';
 
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>创建直播间</h1>
+      <h1 className={styles.title}>编辑直播间</h1>
     </div>
   );
 };
